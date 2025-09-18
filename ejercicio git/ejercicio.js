83feaf49f9f4860379fff1b500f3f7cb4e14d671
vmanzano1711 <vmanzano2803@gmail.com>
2025-09-18 16:25:27 -0500

    cambiado imagen

--- ejercicio git/ejercicio.js
+++ ejercicio git/ejercicio.js
@@ -1,3 +1,6 @@
 saludar=function(){
+    let cmpImagenSaludo;
+    cmpImagenSaludo=document.getElementById("imgSaludo")
     console.log("ingresar a saludar");
-}
+    cmpImagenSaludo.src="./imagenes/buen-trabajo.gif";
+}   
