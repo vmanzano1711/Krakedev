4c022255c739a1c233d342ea141f11f318f39659
vmanzano1711 <vmanzano2803@gmail.com>
2025-09-20 23:05:43 -0500

    cambios en recuperarint y recuperar float

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -3,6 +3,8 @@ saludar=function(){
     let nombre=recuperarTexto("txtNombre");
     // recuperar valor apellido 
     let apellido=recuperarTexto("txtApellido");
+    let edad=recuperarInt("txtEdad");
+    let estatura=recuperarFloat("txtEstatura");
 
 }
 
@@ -13,5 +15,18 @@ recuperarTexto=function(idComponente){
     valorIngresado=componente.value;
     return valorIngresado;
     
+}
+
+recuperarInt=function(idComponente){
+    let valorCaja=recuperarTexto(idComponente);
+    let valorEntero=parseInt(valorCaja);
+    return valorEntero;
+
+}
+recuperarFloat=function(idComponente){
+    let valorCaja=recuperarTexto(idComponente);
+    let valorFlotante=parseFloat(valorCaja);
+    return valorFlotante;
+
 }
 
