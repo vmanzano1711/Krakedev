a7d14fb8263446ce2dbd0d983e473f2425e99651
vmanzano1711 <vmanzano2803@gmail.com>
2025-09-20 22:20:06 -0500

    cambio en reto 25

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -1,7 +1,15 @@
+saludar=function(){ 
+    //recuperarel valorde la caja de texto 
+    let nombre=recuperarTexto("txtNombre");
+    // recuperar valor apellido 
+    let apellido=recuperarTexto("txtApellido");
+
+}
+
 recuperarTexto=function(idComponente){
     let componente;
     let valorIngresado;
-    componente=document.getElementById(idcompnente);
+    componente=document.getElementById(idComponente);
     valorIngresado=componente.value;
     return valorIngresado;
     
