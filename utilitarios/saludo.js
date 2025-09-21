3411718fc06707255d29a2656f811362d5698324
vmanzano1711 <vmanzano2803@gmail.com>
2025-09-20 21:56:05 -0500

    utilitariosvm

---
+++ utilitarios/saludo.js
@@ -0,0 +1,9 @@
+recuperarTexto=function(idComponente){
+    let componente;
+    let valorIngresado;
+    componente=document.getElementById(idcompnente);
+    valorIngresado=componente.value;
+    return valorIngresado;
+    
+}
+
