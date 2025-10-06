77febb7bd18be609dd459fbc41f34724c577aa23
vmanzano1711 <vmanzano2803@gmail.com>
2025-10-06 17:21:49 -0500

    Numeros Impares

---
+++ JUEGO/Bucles/ejemplosfor.js
@@ -0,0 +1,63 @@
+mostrarNumero=function(){
+    console .log("antes del for");
+    for (let i=0;i<4;i++){
+        console.log(i);
+    }
+
+    
+console.log("despeues del for");
+}
+
+mostrarNumero2=function(){
+    for(let indice=1;indice<=5;indice++){
+        console.log(indice);
+    }
+
+    console.log("despeues del for");
+}
+
+mostrarPares=function(){
+    console .log("antes del for")
+    for(let x=2;x<=10;x+=2){
+        console.log(x);
+
+
+    }
+    console.log("despeues del for");
+}
+
+mostrarInverso=function(){
+    console.log("Antes del for");
+    for (let i=10;i>=0;i--){
+        console.log(i);
+
+    }
+     console.log("despeues del for");
+}
+hakearNasaPelis=function(){
+    //>HAKEAR NASA 0% ........100%
+    for (let porcentaje=10;porcentaje>=0;porcentaje-=1){
+       
+        console.log("hakeando nasa"+ porcentaje+"%");
+
+    }
+     console.log("la nasa ha sido hakeada");
+}
+
+mostrarImpares=function(){
+    console.log("Antes del for");
+    for (let impares=1;impares<=21;impares+=2){
+
+        console.log("Impares"+impares);
+    }
+    
+   console.log("despues del for");
+}
+
+
+
+
+
+
+
+
