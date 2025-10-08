8b8fd73e9d021020bcec53e3a8be07716863ca61
vmanzano1711 <vmanzano2803@gmail.com>
2025-10-07 23:34:37 -0500

    PASO CERO

--- Juego del Ahorcado/ahorcado.js
+++ Juego del Ahorcado/ahorcado.js
@@ -1,4 +1,5 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+//// paso cero en mayusculas
 function esMayuscula(caracter) {
     let codigo = caracter.charCodeAt(0);
     return (codigo >= 65 && codigo <= 90);
