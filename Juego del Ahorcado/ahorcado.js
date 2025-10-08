6edacd21916ddbcefbd659a823ff2f0d53d9cf17
vmanzano1711 <vmanzano2803@gmail.com>
2025-10-07 23:37:23 -0500

    PASO 1

--- Juego del Ahorcado/ahorcado.js
+++ Juego del Ahorcado/ahorcado.js
@@ -4,3 +4,26 @@ function esMayuscula(caracter) {
     let codigo = caracter.charCodeAt(0);
     return (codigo >= 65 && codigo <= 90);
 }
+/// paso 1 palabra secreta 
+let palabraSecreta = "";
+
+function guardarPalabra() {
+    let palabra = document.getElementById('txtSecreta').value;
+    
+    // Validar que tenga 5 caracteres
+    if (palabra.length !== 5) {
+        alert("Debe ingresar una palabra de 5 letras mayúsculas");
+        return;
+    }
+    
+    // Validar que sean mayúsculas
+    for (let i = 0; i < palabra.length; i++) {
+        if (!esMayuscula(palabra[i])) {
+            alert("Debe ingresar una palabra de 5 letras mayúsculas");
+            return;
+        }
+    }
+    
+    palabraSecreta = palabra;
+    console.log("Palabra guardada:", palabraSecreta);
+}
